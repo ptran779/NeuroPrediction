d9cabe5433eedc3ac2b7bba39dacf8e6e5a2d3f6
ptran779 <phong6900user@gmail.com>
2023-11-15 20:35:08 -0600

    update on model. Now with validation. Tuning still suck, model WILL overtrain if step_per_epoch too high -- need more feature incoporate.

---
+++ src/DataManager.py
@@ -0,0 +1,250 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+class DataManager:
+    """
+    Handle DNN dataset and apply modification needed
+    """
+    def __init__(self):
+        self._dat_main = None
+        self._dat_lboard = None
+        self._dat_feat = None
+        self._dat_morp = None
+        self.BATCH_SIZE = 0
+        self.EPOCHS = 0
+        self.BUFFER_SIZE = 0
+        self._train_ds = None
+        self._val_ds = None
+        self._test_ds = None
+        self.step_per_epoch = None
+        self.lea_ds = None  # wip. just a bunch of feature for now
+
+    def config(self, batch_size: int, epochs: int, buffer_size: int):
+        self.BATCH_SIZE = batch_size
+        self.EPOCHS = epochs
+        self.BUFFER_SIZE = buffer_size
+
+    def load_dat_feat(self, fname: str):
+        self._dat_feat = pd.read_csv(fname)
+        # join all morph_embed_i columns into a single np.array column
+        self._dat_feat["feature_weights"] = (
+            self._dat_feat.filter(regex="feature_weight_")
+            .sort_index(axis=1)
+            .apply(lambda x: np.array(x), axis=1)
+        )
+        self._dat_feat.drop(self._dat_feat.filter(regex="feature_weight_").columns, axis=1, inplace=True)
+
+    def load_dat_morp(self, fname: str):
+        self._dat_morp = pd.read_csv(fname)
+        self._dat_morp["morph_embeddings"] = (
+            self._dat_morp.filter(regex="morph_emb_")
+            .sort_index(axis=1)
+            .apply(lambda x: np.array(x), axis=1)
+        )
+        self._dat_morp.drop(self._dat_morp.filter(regex="morph_emb_").columns, axis=1, inplace=True)
+
+    def load_dat_main(self, fname: str):
+        if not self.dataIsLoadedDat():
+            raise Exception("please load morp and feature data first")
+        self._dat_main = pd.read_csv(fname)
+
+        self._dat_main = (
+            self._dat_main.merge(
+                self._dat_feat.rename(columns=lambda x: "pre_" + x),
+                how="left",
+                validate="m:1",
+                copy=False,
+            )
+            .merge(
+                self._dat_feat.rename(columns=lambda x: "post_" + x),
+                how="left",
+                validate="m:1",
+                copy=False,
+            )
+            .merge(
+                self._dat_morp.rename(columns=lambda x: "pre_" + x),
+                how="left",
+                validate="m:1",
+                copy=False,
+            )
+            .merge(
+                self._dat_morp.rename(columns=lambda x: "post_" + x),
+                how="left",
+                validate="m:1",
+                copy=False,
+            )
+        )
+
+        dm1, dm2 = len(self._dat_main['pre_morph_embeddings'][0]), len(self._dat_main['post_morph_embeddings'][0])
+        for i, r in self._dat_main.iterrows():
+            if isinstance(r['pre_morph_embeddings'], float):
+                self._dat_main.at[i, 'pre_morph_embeddings'] = np.zeros(dm1)
+            if isinstance(r['post_morph_embeddings'], float):
+                self._dat_main.at[i, 'post_morph_embeddings'] = np.zeros(dm2)
+
+    def load_dat_lboard(self, fname: str):
+        if not self.dataIsLoadedDat():
+            raise Exception("please load morp and feature data first")
+
+        self._dat_lboard = pd.read_csv(fname)
+
+        self._dat_lboard = (
+            self._dat_lboard.merge(
+                self._dat_feat.rename(columns=lambda x: "pre_" + x),
+                how="left",
+                validate="m:1",
+                copy=False,
+            )
+            .merge(
+                self._dat_feat.rename(columns=lambda x: "post_" + x),
+                how="left",
+                validate="m:1",
+                copy=False,
+            )
+            .merge(
+                self._dat_morp.rename(columns=lambda x: "pre_" + x),
+                how="left",
+                validate="m:1",
+                copy=False,
+            )
+            .merge(
+                self._dat_morp.rename(columns=lambda x: "post_" + x),
+                how="left",
+                validate="m:1",
+                copy=False,
+            )
+        )
+
+        dm1, dm2 = len(self._dat_lboard['pre_morph_embeddings'][0]), len(self._dat_lboard['post_morph_embeddings'][0])
+        for i, r in self._dat_lboard.iterrows():
+            if isinstance(r['pre_morph_embeddings'], float):
+                self._dat_lboard.at[i, 'pre_morph_embeddings'] = np.zeros(dm1)
+            if isinstance(r['post_morph_embeddings'], float):
+                self._dat_lboard.at[i, 'post_morph_embeddings'] = np.zeros(dm2)
+
+    def dataIsLoadedDat(self):
+        return not (self._dat_morp is None or self._dat_feat is None)
+
+    def prepTrainValTest(self, w_trn, w_val, w_tst, random_state=0):
+        # split data
+        tmp = w_tst/(w_trn+w_val+w_tst)
+        dm_df, test_df = train_test_split(self._dat_main, test_size=tmp, random_state=random_state)
+        tmp = w_val/(w_val+w_trn)
+        train_df, val_df = train_test_split(dm_df, test_size=tmp, random_state=random_state)
+
+        # process
+        f_trn, l_trn = DataManager.processFromDf(train_df)
+        f_val, l_val = DataManager.processFromDf(val_df)
+        f_tst, l_tst = DataManager.processFromDf(test_df)
+
+        # split positive and negative sample and make them
+        pos_ds = self.makeDs(f_trn, l_trn, np.where(l_trn == 1))
+        neg_ds = self.makeDs(f_trn, l_trn, np.where(l_trn == 0))
+        resampled_ds = tf.data.Dataset.sample_from_datasets([pos_ds, neg_ds], weights=[.5, .5])
+
+        self._train_ds = resampled_ds.batch(self.BATCH_SIZE).prefetch(2)
+        self.step_per_epoch = np.ceil(2.0 * (len(l_trn) - np.count_nonzero(l_trn)) / self.BATCH_SIZE)
+
+        self._val_ds = self.makeDs(f_val, l_val)
+        self._test_ds = self.makeDs(f_tst, l_tst)
+
+    def loadLeader(self):
+        self.lea_ds, _ = self.processFromDf(self._dat_lboard)
+
+    def getTrainDs(self):
+        return self._train_ds
+    
+    def getValDs(self):
+        return self._val_ds
+    
+    def getTestDs(self):
+        return self._test_ds
+
+    @staticmethod
+    def oneHotConverter(raw):
+        """
+        convert a 1D array to one-hot-coding
+        :param raw: an array
+        :return: 2D matrix
+        """
+        cls = np.unique(raw)
+        out = np.zeros([len(raw), len(cls)])
+        cls_look_up = {item: index for index, item in enumerate(cls)}  # convert array to dict
+        for c, i in enumerate(raw):
+            out[c, cls_look_up[i]] = 1  # mark target code to 1
+        return out
+
+    @staticmethod
+    def splitData(arr_len, ratio, seed=-1):
+        """
+        Split data to test and train with numpy random seeding.
+        :param arr_len: length of array need split
+        :param ratio: 0-1 of item to split, round down to first array
+        :param seed: use to control random state -- set to -1 to disable
+        :return: tuple of 2 arrays contain index that has been split
+        """
+        # set random state if used
+        if seed != -1:
+            rng = np.random.default_rng(seed)
+            shuffled_indices = rng.permutation(arr_len)
+        else:
+            shuffled_indices = np.random.permutation(arr_len)
+        # split
+        split_index = int(arr_len * ratio)
+        return shuffled_indices[:split_index], shuffled_indices[split_index:]
+
+    @staticmethod
+    def processFromDf(raw_df, skip_y=0):
+        indat1 = np.copy(raw_df[['adp_dist', 'pre_oracle', 'post_oracle', 'pre_skeletal_distance_to_soma',
+                                 'post_skeletal_distance_to_soma']].values)
+        raw = raw_df.pre_brain_area.values
+        indat2 = DataManager.oneHotConverter(raw)
+
+        raw = raw_df.post_brain_area.values
+        indat3 = DataManager.oneHotConverter(raw)
+
+        raw = raw_df.compartment.values
+        indat4 = DataManager.oneHotConverter(raw)
+
+        indat5 = np.vstack(raw_df.pre_morph_embeddings.values)
+        indat6 = np.vstack(raw_df.post_morph_embeddings.values)
+
+        labels = None
+        if not skip_y:
+            raw = raw_df.connected.values
+            labels = DataManager.oneHotConverter(raw)[:, 1]
+
+        # Normalization
+        # transform bulk data
+        arr = np.transpose(indat1)
+        arr[0] = arr[0] / 500  # scale down large value
+        arr[1] = arr[1] * 10  # upscale
+        arr[2] = arr[2] * 10  # upscale
+        arr[3] = arr[3] / 250000  # scale down large value
+        arr[4] = arr[4] / 180000  # scale down large value`
+        return (indat1, indat2, indat3, indat4, indat5, indat6), labels
+
+    def makeDs(self, features_list, labels, index=None):
+        """
+        :param features_list: [feature1, feature2, ...]
+        :param labels: output label
+        :param index: position for this dataset -- training need index to split pos and neg sample for rabalancing, 
+        val and test does not. I know it's wrong in every shape and form, but I'm lazy
+        :return: tf.dataset
+        """
+        used_features = {}
+        ds = None
+        if index is None:
+            for c, feature in enumerate(features_list):
+                used_features["Feature {}".format(c)] = feature
+            ds = tf.data.Dataset.from_tensor_slices((used_features, labels)).cache()
+            ds = ds.batch(self.BATCH_SIZE).prefetch(2)
+        else:
+            for c, feature in enumerate(features_list):
+                used_features["Feature {}".format(c)] = feature[index]
+            ds = tf.data.Dataset.from_tensor_slices((used_features, labels[index]))
+            ds = ds.shuffle(self.BUFFER_SIZE).repeat()
+        return ds
--- src/NeroClassifier.py
+++ src/NeroClassifier.py
@@ -3,29 +3,36 @@ from tensorflow import keras
 from keras.utils import plot_model
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from DataManager import *
 import matplotlib.pyplot as plt
 
 from util import basic_func
 import seaborn as sns
 
+SEED = 12
 BATCH_SIZE = 32
-EPOCHS = 1000
-BUFFER_SIZE = 1000
+EPOCHS = 200
+BUFFER_SIZE = 10000
+TRN = 0.6
+VAL = 0.2
+TST = 0.2
 METRICS = [
-      keras.metrics.BinaryCrossentropy(name='cross entropy'),  # same as model's loss
-      keras.metrics.MeanSquaredError(name='Brier score'),
-      keras.metrics.TruePositives(name='tp'),
-      keras.metrics.FalsePositives(name='fp'),
-      keras.metrics.TrueNegatives(name='tn'),
-      keras.metrics.FalseNegatives(name='fn'),
-      keras.metrics.BinaryAccuracy(name='accuracy'),
-      keras.metrics.Precision(name='precision'),
-      keras.metrics.Recall(name='recall'),
-      keras.metrics.AUC(name='auc'),
-      keras.metrics.AUC(name='prc', curve='PR'), # precision-recall curve
+    keras.metrics.BinaryCrossentropy(name='cross entropy'),  # same as model's loss
+    keras.metrics.MeanSquaredError(name='Brier score'),
+    keras.metrics.TruePositives(name='tp'),
+    keras.metrics.FalsePositives(name='fp'),
+    keras.metrics.TrueNegatives(name='tn'),
+    keras.metrics.FalseNegatives(name='fn'),
+    keras.metrics.BinaryAccuracy(name='accuracy'),
+    keras.metrics.Precision(name='precision'),
+    keras.metrics.Recall(name='recall'),
+    keras.metrics.AUC(name='auc'),
+    keras.metrics.AUC(name='prc', curve='PR'),  # precision-recall curve
 ]
-TRN_TST_RATIO = 0.8
+
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_prc', verbose=1, patience=10, mode='max',
+                                                  restore_best_weights=True)
+keras.utils.set_random_seed(SEED)
 
 # activate GPU accelerator
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -33,61 +40,83 @@ if gpus:
     # tf.config.experimental.set_memory_growth(gpus[0], True)  # Set memory growth to minimize GPU memory usage
     tf.config.experimental.set_visible_devices(gpus[0], 'GPU')  # Use the first GPU
 
-# input preparation -- should make a class to manage this
-# read in data
-data = pd.read_csv("./resource/train_data2.csv")
+# loss fun:
+# need constrative learning loss func
 
-# load in additional features for each neuron
-feature_weights = pd.read_csv("./resource/feature_weights.csv")
-morph_embeddings = pd.read_csv("./resource/morph_embeddings.csv")
+# this is wrong
+def contrastive_loss(y_true, y_pred, margin=0.5):
+    # y_true: true labels (1 for similar, 0 for dissimilar)
+    # y_pred: predicted distances between pairs
+    # Squared Euclidean distance
+    squared_pred = tf.square(y_pred)
+    # Contrastive loss formula
+    loss = y_true * squared_pred + (1 - y_true) * tf.maximum(margin - y_pred, 0)
+    # Compute mean loss
+    loss = 0.5 * tf.reduce_mean(loss)
+    return loss
 
-# join all feature_weights
-feature_weights["feature_weights"] = (
-    feature_weights.filter(regex="feature_weight_")
-    .sort_index(axis=1)
-    .apply(lambda x: np.array(x), axis=1)
-)
-# delete the feature_weight_i columns
-feature_weights.drop(
-    feature_weights.filter(regex="feature_weight_").columns, axis=1, inplace=True
-)
 
-# join all morph_embed_i columns into a single np.array column
-morph_embeddings["morph_embeddings"] = (
-    morph_embeddings.filter(regex="morph_emb_")
-    .sort_index(axis=1)
-    .apply(lambda x: np.array(x), axis=1)
-)
-# delete the morph_embed_i columns
-morph_embeddings.drop(
-    morph_embeddings.filter(regex="morph_emb_").columns, axis=1, inplace=True
-)
-data = (
-    data.merge(
-        feature_weights.rename(columns=lambda x: "pre_" + x),
-        how="left",
-        validate="m:1",
-        copy=False,
-    )
-    .merge(
-        feature_weights.rename(columns=lambda x: "post_" + x),
-        how="left",
-        validate="m:1",
-        copy=False,
-    )
-    .merge(
-        morph_embeddings.rename(columns=lambda x: "pre_" + x),
-        how="left",
-        validate="m:1",
-        copy=False,
-    )
-    .merge(
-        morph_embeddings.rename(columns=lambda x: "post_" + x),
-        how="left",
-        validate="m:1",
-        copy=False,
-    )
-)
+def plot_metrics(history):
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    metrics = ['loss', 'prc', 'precision', 'recall']
+    for n, metric in enumerate(metrics):
+        name = metric.replace("_", " ").capitalize()
+        plt.subplot(2, 2, n + 1)
+        plt.plot(history.epoch, history.history[metric], color=colors[0], label='Train')
+        plt.plot(history.epoch, history.history['val_' + metric],
+                 color=colors[0], linestyle="--", label='Val')
+        plt.xlabel('Epoch')
+        plt.ylabel(name)
+        if metric == 'loss':
+            plt.ylim([0, plt.ylim()[1]])
+        elif metric == 'auc':
+            plt.ylim([0.8, 1])
+        else:
+            plt.ylim([0, 1])
+
+        plt.legend()
+
+
+# Input Data
+datamanager = DataManager()
+datamanager.config(BATCH_SIZE, EPOCHS, BUFFER_SIZE)
+datamanager.load_dat_feat("./resource/feature_weights.csv")
+datamanager.load_dat_morp("./resource/morph_embeddings.csv")
+datamanager.load_dat_main("./resource/train_data2.csv")
+datamanager.prepTrainValTest(TRN, VAL, TST, SEED)
+# Create a model
+in0 = keras.layers.Input(shape=(5,), dtype='float32', name='Feature 0')
+in1 = keras.layers.Input(shape=(3,), dtype='float32', name='Feature 1')
+in2 = keras.layers.Input(shape=(3,), dtype='float32', name='Feature 2')
+in3 = keras.layers.Input(shape=(7,), dtype='float32', name='Feature 3')
+in4 = keras.layers.Input(shape=(32,), dtype='float32', name='Feature 4')
+in5 = keras.layers.Input(shape=(32,), dtype='float32', name='Feature 5')
+merged = keras.layers.Concatenate(axis=1)([in0, in1, in2, in3, in4, in5])
+dense1 = keras.layers.Dense(64, activation=keras.activations.gelu)(merged)  # try gelu
+dense2 = keras.layers.Dense(32, activation=keras.activations.gelu)(dense1)
+drop1 = keras.layers.Dropout(0.2)(dense2)
+dense3 = keras.layers.Dense(16, activation=keras.activations.gelu)(drop1)
+output = keras.layers.Dense(1, activation=keras.activations.sigmoid)(dense3)
+model = keras.models.Model([in0, in1, in2, in3, in4, in5], output)
+
+plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=METRICS)  # Using binary cross-entropy loss
+# model.compile(optimizer='adam', loss=contrastive_loss, metrics=METRICS)  # Using contrastive loss
+# Train the model
+history = model.fit(
+    datamanager.getTrainDs(),
+    epochs=EPOCHS,
+    steps_per_epoch=datamanager.step_per_epoch,
+    callbacks=[early_stopping],
+    validation_data=(datamanager.getValDs()))
+
+hist_test = model.evaluate(datamanager.getTestDs())
+#
+plot_metrics(history)
+print("[{} {}]\n[{} {}]".format(hist_test[3], hist_test[4], hist_test[5], hist_test[6]))
+print((hist_test[3] / (hist_test[3] + hist_test[6]) + hist_test[5] / (hist_test[5] + hist_test[4])) / 2)
+
 
 ############################ Adding the cosine similarity column##################### BEN
 #cosine similarity function
@@ -107,12 +136,12 @@ data["fw_similarity"] = data.apply(row_feature_similarity, axis=1)
 # from sklearn.decomposition import PCA
 
 # # Exclude non-numeric and non-categorical columns from the PCA - I guess you don't have any here. So you can remove this part
-# exclude_cols = ['ID', 
-#                 'pre_feature_weights', 
-#                 'post_feature_weights', 
-#                 'pre_morph_embeddings', 
-#                 'post_morph_embeddings', 
-#                 'projection_group', 
+# exclude_cols = ['ID',
+#                 'pre_feature_weights',
+#                 'post_feature_weights',
+#                 'pre_morph_embeddings',
+#                 'post_morph_embeddings',
+#                 'projection_group',
 #                 'connected',
 #                 'is_left',
 #                 'is_right',
@@ -137,141 +166,3 @@ data["fw_similarity"] = data.apply(row_feature_similarity, axis=1)
 
 
 ######################################################
-
-
-# split data to train and test set
-train_df, test_df = train_test_split(data, train_size=TRN_TST_RATIO, random_state=0)
-
-
-
-def process_df(raw_df):
-    # data processing  -- will need a handler for this
-    indat1 = np.copy(raw_df[['adp_dist', 'pre_oracle', 'post_oracle', 'pre_skeletal_distance_to_soma',
-                           'post_skeletal_distance_to_soma']].values)
-    raw = raw_df.pre_brain_area.values
-    indat2 = basic_func.one_hot_convert(raw)
-
-    raw = raw_df.post_brain_area.values
-    indat3 = basic_func.one_hot_convert(raw)
-
-    raw = raw_df.compartment.values
-    indat4 = basic_func.one_hot_convert(raw)
-
-    raw = raw_df.connected.values
-    labels = basic_func.one_hot_convert(raw)[:, 1]
-
-    # Normalization
-    # transform bulk data
-    arr = np.transpose(indat1)
-    arr[0] = arr[0] / 500  # scale down large value
-    arr[1] = arr[1] * 10  # upscale
-    arr[2] = arr[2] * 10  # upscale
-    arr[3] = arr[3] / 250000  # scale down large value
-    arr[4] = arr[4] / 180000  # scale down large value`
-    # sns.boxplot(indat1)
-    # return (np.reshape(indat1, (len(indat1), 5, 1)),
-    #         np.reshape(indat2, (len(indat1), 3, 1),),
-    #         np.reshape(indat3, (len(indat1), 3, 1)),
-    #         np.reshape(indat4, (len(indat1), 7, 1))), labels
-    return  (indat1, indat2, indat3, indat4), labels
-
-
-# process and obtain data for train and test
-f_trn, l_trn = process_df(train_df)
-f_tst, l_tst = process_df(test_df)
-
-pos_sample = np.where(l_trn == 1)
-neg_sample = np.where(l_trn == 0)
-
-
-# compile data and ready for tf
-def make_ds(features_list, labels, index):
-    """
-    :param features_list: [feature1, feature2, ...]
-    :param labels: output label
-    :param index: position for this dataset
-    :return: tf.dataset
-    """
-    used_features = {}
-    for c, feature in enumerate(features_list):
-        used_features["Feature {}".format(c)] = feature[index]
-    ds = tf.data.Dataset.from_tensor_slices((used_features, labels[index]))
-    ds = ds.shuffle(BUFFER_SIZE).repeat()
-    return ds
-
-
-def make_ds_test(features_list, labels):
-    used_features = {}
-    for c, feature in enumerate(features_list):
-        used_features["Feature {}".format(c)] = feature
-    ds = tf.data.Dataset.from_tensor_slices((used_features, labels))
-    ds = ds.batch(BATCH_SIZE).prefetch(2)
-    return ds
-
-
-pos_ds = make_ds(f_trn, l_trn, np.where(l_trn == 1))
-neg_ds = make_ds(f_trn, l_trn, np.where(l_trn == 0))
-
-resampled_ds = tf.data.Dataset.sample_from_datasets([pos_ds, neg_ds], weights=[0.5, 0.5])
-resampled_ds = resampled_ds.batch(BATCH_SIZE).prefetch(2)
-
-resampled_steps_per_epoch = np.ceil(2.0*len(neg_sample)/BATCH_SIZE)
-
-# loss fun:
-# need constrative learning loss func
-# WIP
-
-# Create a model
-in1 = keras.layers.Input(shape=(5,), dtype='float32', name='Feature 0')
-in2 = keras.layers.Input(shape=(3,), dtype='float32', name='Feature 1')
-in3 = keras.layers.Input(shape=(3,), dtype='float32', name='Feature 2')
-in4 = keras.layers.Input(shape=(7,), dtype='float32', name='Feature 3')
-merged = keras.layers.Concatenate(axis=1)([in1, in2, in3, in4])
-dense1 = keras.layers.Dense(12, activation=keras.activations.relu)(merged)      # try gelu
-dense2 = keras.layers.Dense(12, activation=keras.activations.relu)(dense1)
-dense3 = keras.layers.Dense(6, activation=keras.activations.relu)(dense2)
-output = keras.layers.Dense(1, activation=keras.activations.sigmoid)(dense3)
-model = keras.models.Model([in1, in2, in3, in4], output)
-
-# plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=METRICS)  # Using binary cross-entropy loss
-
-# Train the model
-history = model.fit(
-    resampled_ds,
-    epochs=EPOCHS,
-    batch_size=BATCH_SIZE,
-    steps_per_epoch=resampled_steps_per_epoch)
-
-# validation
-val_ds = make_ds_test(f_tst, l_tst)
-
-history_val = model.evaluate(val_ds)
-colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-
-
-def plot_metrics(history):
-  metrics = ['loss', 'prc', 'precision', 'recall']
-  for n, metric in enumerate(metrics):
-    name = metric.replace("_"," ").capitalize()
-    plt.subplot(2,2,n+1)
-    plt.plot(history.epoch, history.history[metric], color=colors[0], label='Train')
-    # plt.plot(history.epoch, history.history['val_'+metric],
-    #          color=colors[0], linestyle="--", label='Val')
-    plt.xlabel('Epoch')
-    plt.ylabel(name)
-    if metric == 'loss':
-      plt.ylim([0, plt.ylim()[1]])
-    elif metric == 'auc':
-      plt.ylim([0.8,1])
-    else:
-      plt.ylim([0,1])
-
-    plt.legend()
-
-
-plot_metrics(history)
-print(history_val.history)
-
-
